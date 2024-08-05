75efd996c8c8dffba63617f7333382ab285d8ecf
sogood17 <sogoodpiano@gmail.com>
2024-08-05 21:38:55 +0900

    making router

---
+++ myContacts/routes/contactRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+
+//Read and create all contacts
+router.route("/")
+.get((req, res) => {
+  res.send("Contacts Page!");
+  })
+.post((req, res) => {
+  const {name, email, phone} = req.body;
+  if (!name || !email || !phone) {
+    return res.send("Required value not entered.")
+  }
+
+  res.send("Create Contact");
+  })
+
+//Read 1 contact
+router.route("/:id")
+.get((req, res) => {
+  res.send("View Contact for ID: " + req.params.id);
+  })
+//Modify 1 contact
+.put((req, res) => {
+  res.send("Update Contact for ID: " + req.params.id);
+  })
+//Delete 1 contact
+.delete((req, res) => {
+  res.send("Delete Contact for ID: " + req.params.id);
+  })
+
+//Using GET method without middleware
+// app.get("/contacts", (req, res) => {
+//   res.send("Contacts Page!");
+// })
+
+module.exports = router;
