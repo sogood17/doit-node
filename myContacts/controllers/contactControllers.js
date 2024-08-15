347fd847147f1d654a3785d70e461e07c40e2e42
sogood17 <sogoodpiano@gmail.com>
2024-08-15 20:58:16 +0900

    DB CRUD function updated

--- myContacts/controllers/contactControllers.js
+++ myContacts/controllers/contactControllers.js
@@ -1,51 +1,57 @@
-//GET contacts/
+const Contact = require("../models/contactModel");
 const asyncHandler = require("express-async-handler");
 
+//GET contacts/
 const getAllContacts = asyncHandler(async(req, res) => {
-  try {
-    res.send("Contacts Page!");
-    console.log(req.body);
-  } catch (err) {
-    console.log(err);
-  }
+  const allContacts = await Contact.find();
+  res.json(allContacts);
 });
 //POST contacts/
 const postAllContacts = asyncHandler(async(req, res) => {
-  try {
-    const {name, email, phone} = req.body;
-    if (!name || !phone) {
-      return res.send("Required value not entered.");
-    }
-    res.send("Create Contact!");
-
-  } catch (err) {
-    console.log(err);
+  const {name, email, phone} = req.body;
+  if (!name || !phone) {
+    return res.send("Required value not entered.");
+  } else {
+  const newContact = await Contact.create({name, email, phone})
+  res.send("Create Contact!");
   }
 });
 //GET contacts/:id
 const getContact = asyncHandler(async(req, res) => {
-  try {
-    res.send("View Contact for ID:" + req.params.id);
-  } catch (err) {
-    console.log(err);
-  }
+  const {id}=req.params;
+  const contactById = await Contact.findById(id);
+  res.json(contactById);
 });
 //PUT contacts/:id
 const updateContact = asyncHandler(async(req, res) => {
-  try {
-    const {name, email, phone} = req.body;
-    if (!name && !email && !phone) {
-      res.send("At least one value is required.");
-    } else {
-      res.send(req.params.id + " is Updated.");
+  const {id}=req.params;
+  const contactById = await Contact.findById(id);
+  const {name, email, phone} = req.body;
+  if (!name && !email && !phone) {
+    res.send("At least one value is required.");
+  } else {
+    if (name) {
+    contactById.name=name;
     }
-  } catch (err) {
-    console.log(err);
+    if (email) {
+    contactById.email=email;
+    }
+    if (phone) {
+    contactById.phone=phone;
+    }
+    contactById.save();
+    res.send(id + " is Updated.");
   }
 });
 //DELETE contacts/:id
 const deleteContact = asyncHandler(async(req, res) => {
-  res.send("Delete Contact for ID: " + req.params.id);
+  const {id}=req.params;
+  const contactById = await Contact.findById(id);
+  if (!contactById) {
+    throw new Error("Contact nof found");
+  }
+  const deletedContact = await Contact.deleteOne()
+  res.send("Delete Contact for ID: " + id);
 });
 
 
