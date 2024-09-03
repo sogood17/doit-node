73a1b3d615ec64f8aedbeec21285d225616da6d8
sogood17 <sogoodpiano@gmail.com>
2024-09-03 21:57:26 +0900

    set login page

--- myContacts/routes/loginRoutes.js
+++ myContacts/routes/loginRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {getLogin} = require("../controllers/loginControllers");
+const getLogin = require("../controllers/loginControllers");
 
 router.route("/").get(getLogin);
 
