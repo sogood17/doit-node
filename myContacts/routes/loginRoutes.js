04622bc92433edc63b78559f6ad93bda55933c91
sogood17 <sogoodpiano@gmail.com>
2024-09-03 09:00:26 +0900

    make loginRoutes.js

---
+++ myContacts/routes/loginRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const {getLogin} = require("../controllers/loginControllers");
+
+router.route("/").get(getLogin);
+
+module.exports = router;
