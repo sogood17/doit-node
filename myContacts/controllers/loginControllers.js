c39402a87a99a2077876b31073a44bd51a7d92fe
sogood17 <sogoodpiano@gmail.com>
2024-09-04 10:47:56 +0900

    register page rendering

--- myContacts/controllers/loginControllers.js
+++ myContacts/controllers/loginControllers.js
@@ -7,4 +7,23 @@ const getLogin = asyncHandler(async(req, res) => {
   res.render("home");
 })
 
-module.exports = getLogin;
+//POST login
+//POST /
+const loginUser = asyncHandler(async(req, res)=> {
+  const {username, password} = req.body;
+
+  if (username === "admin" && password === "1234") {
+    res.send("login is successful");
+  } else {
+    res.send("Please check your ID or password");
+  }
+})
+
+//GET register page
+//GET /register
+const getRegister = asyncHandler(async(req, res)=> {
+  res.render("register");
+})
+
+
+module.exports = {getLogin, loginUser, getRegister};
--- myContacts/routes/loginRoutes.js
+++ myContacts/routes/loginRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const getLogin = require("../controllers/loginControllers");
+const {getLogin, loginUser, getRegister} = require("../controllers/loginControllers");
 
-router.route("/").get(getLogin);
+router.route("/").get(getLogin).post(loginUser);
+router.route("/register").get(getRegister);
 
 module.exports = router;
